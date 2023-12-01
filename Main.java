4abe65ae6e29c718503ab0cdf188ee267912d7a6
bfoley05 <142286051+bfoley05@users.noreply.github.com>
2023-11-30 19:32:20 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,14 +12,17 @@ public class Main {
     // adjust file path to your own path. Below is an exmaple for macs. Otherwise use C:\Users\name\Documents\CPSC_Courses\MP4\Medicines.csv
     public static final String MEDICINE_CSV = "/Users/brandonfoley/Documents/CPSC_Courses/MP4/Medicines.csv";
     public static final String HR_CSV = "/Users/brandonfoley/Documents/CPSC_Courses/MP4/FakeHRs.csv";
+    public static final String USER_CSV = "/Users/brandonfoley/Documents/CPSC_Courses/MP4/UserCSV.csv";
     public static void main(String[] args){
         System.out.println("\n\n\n");
         File medicinesFile = new File(MEDICINE_CSV);
         File HRFile = new File(HR_CSV);
         List<List<String>> HRData = HRFile.getData();
         List<List<String>> data = medicinesFile.getData();
-        System.out.println(data.get(0));
         Users u = new Users();
+        displayLoadingScreen();
+        simulateDatabaseLoading();
+        System.out.println("Medical database loaded successfully!\n");
 
         int input = 1;
         while(input > 0){
@@ -88,6 +91,82 @@ public class Main {
             e.printStackTrace();
         }
         return lineCount;
+    }
+
+    private static void displayLoadingScreen() {
+        System.out.print("\033[H\033[2J");
+        printLogo();
+        System.out.println("Intializing...");
+        try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        // Simulate loading animation with a progress bar
+        int totalProgressBars = 50;
+        for (int i = 0; i < totalProgressBars; i++) {
+            try {
+                Thread.sleep(150);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.print("\033[H\033[2J");
+            printLogo();
+            System.out.print("Loading [");
+            int progress = (int) (((double) i / (totalProgressBars - 1)) * 100);
+            int barCount = (int) (((double) i / (totalProgressBars - 1)) * totalProgressBars);
+            for (int j = 0; j < barCount; j++) {
+                System.out.print("=");
+            }
+            for (int j = barCount; j < totalProgressBars; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("] " + progress + "%");
+        }
+        System.out.println();
+        try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        System.out.print("\033[H\033[2J");
+    }
+
+    private static void simulateDatabaseLoading() {
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void printLogo(){
+        System.out.println("\n\n");
+        System.out.println("                             &&&&&&&&&&&&&&&&&&                                 ");
+        System.out.println("                             &&&&&&&&&&&&&&&&&&                                 ");
+        System.out.println("                             &&&&&&&&&&&&&&&&&&                                 ");
+        System.out.println("                             &&&&&&&&&&&&&&&&&&                                 ");
+        System.out.println("                             &&&&&&&&&&&&&&&&&&                                 ");
+        System.out.println("                             &&&&&&&&&&&&&&&&&&       /&                        ");
+        System.out.println("                             &&&&&&&&&&&&&&(       #&&&                         ");
+        System.out.println("          &&&&&&&&&&&&&&&&&&&&&&&&&&&&&,       #&&&&&   (&&&&&&&&&              ");
+        System.out.println("          &&&&&&&&&&&&&&&&&&&&&&&&&&       &&&&&&&    (((&&&&&&&&&              ");
+        System.out.println("          &&&&&&&&&&&&&&&&&&&&&&&      (&&&&&&&  *((((((&&&&&&&&&&              ");
+        System.out.println("          &&&&&&&&&&&&&&&&&&&&(     #&&&&&&((((((((((((&&&&&&&&&&&              ");
+        System.out.println("          &&&&&&&&&&&&&&&&&&#     &&&&&%(((((((((((((&&&&&&&&&&&&&              ");
+        System.out.println("          &&&&&&&&&&&&&&&&&     &&&&&((((((((((((((&&&&&& &&&&&&&&              ");
+        System.out.println("          &&&&&&&&&&&&&&&&    &&&&%(((((((((((((&&&&&&&  %&&&&&&&&              ");
+        System.out.println("          &&&&&&&&&&&&&&&    &&&&((((((((((((&&&&&&&    &&&&&&&&&&              ");
+        System.out.println("                            &&&#(((((((((&&&&&&&%                               ");
+        System.out.println("                           ,&&(((((((#&&&&&&*                                   ");
+        System.out.println("                           &&%(((((&&&&&.                                       ");
+        System.out.println("                           &&((((&&&&      .&&&                                 ");
+        System.out.println("                           .%((&&&      &&&&&&&                                 ");
+        System.out.println("                            ((&&.    &&&&&&&&&&                                 ");
+        System.out.println("                            .%&    %&&&&&&&&&&&                                 ");
+        System.out.println("                             &    @&&&&&&&&&&&&                                 ");
+        System.out.println("\n\n");
     }
 
 }
