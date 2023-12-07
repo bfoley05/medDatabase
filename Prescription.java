d47c11b7b1f39098f746d50a29b2270d029323ac
bfoley05 <142286051+bfoley05@users.noreply.github.com>
2023-12-06 16:55:22 -0800

    Update Prescription.java

--- Prescription.java
+++ Prescription.java
@@ -1,7 +1,21 @@
 package MP4;
 
+/**
+ * The prupose is to make prescription medince is the child
+ * @author Brandon Foley
+ * @version 1.0
+ */
+
 public class Prescription extends Medicine {
 
+    /**
+     * ovearloaded constructor connects to parent
+     * @param id
+     * @param name
+     * @param expirationDate
+     * @param chemicalCompound
+     * @param drugInteractions
+     */
     public Prescription(int id, String name, String expirationDate, String chemicalCompound, String drugInteractions) {
         super(id, name, expirationDate, chemicalCompound, drugInteractions);
     }
@@ -11,6 +25,10 @@ public class Prescription extends Medicine {
         return "Prescription medicine: " + getMedicine();
     }
 
+    /**
+     * adds medicine to the csv
+     * @return boolean when added
+     */
     public boolean addMedicine(){
         //id,medicine_name,expiration_date,over_the_counter,chemical_compound,drug_interactions,additional_info
         String[] data = new String[7];
@@ -25,6 +43,11 @@ public class Prescription extends Medicine {
         return true;
     }
 
+    /**
+     * method to remove medicne from csv
+     * @param id int id to be removed
+     * @return true when removed
+     */
     public static boolean removeMedicine(int id){
         removeRowFromCSV(id);
         return true;
