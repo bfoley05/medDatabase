efb0f7da1335d3b78d481f444b943073279f852f
bfoley05 <142286051+bfoley05@users.noreply.github.com>
2023-12-06 16:50:15 -0800

    Update File.java

--- File.java
+++ File.java
@@ -6,16 +6,37 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The prupose is to read data from a file
+ * @author Brandon Foley
+ * @version 1.0
+ */
+
 public class File {
     List<List<String>> data;
+
+    /**
+     * Overloaded constructor takes in the file path
+     * @param f String of the file path
+     */
     public File(String f){
         data = readFile(f);
     }
 
+    /**
+     * Getter for the 2D array of data taken from the CSV
+     * @return data taken from CSV
+     */
     public List<List<String>> getData(){
         return data;
     }
 
+    /**
+     * Takes in a String that is the file path and parses through it
+     * to put it into a 2D array
+     * @param f String of the filepath
+     * @return the 2D array taken from the CSV
+     */
     public static List<List<String>> readFile(String f) {
         List<List<String>> data = new ArrayList<>();
 
@@ -36,9 +57,14 @@ public class File {
         return data;
     }
 
+    /**
+     * Parses through each seperate line of the CSV
+     * @param line takes in one line of the CSV to parse through it
+     * @return the 1D array of the line that has been parsed
+     */
     public static List<String> parseCSVLine(String line) {
         List<String> values = new ArrayList<>();
-        String[] tokens = line.split(",", 4);  // Split the line into first 3 values and the rest
+        String[] tokens = line.split(",", 4);
         for (int i = 0; i < 3; i++) {
             values.add(tokens[i].trim());
         }
