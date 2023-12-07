49e87649a954b7796737e75dff7f56900d5facb6
bfoley05 <142286051+bfoley05@users.noreply.github.com>
2023-12-06 16:49:54 -0800

    Update Counter.java

--- Counter.java
+++ Counter.java
@@ -1,5 +1,9 @@
 package MP4;
-
+/**
+ * The prupose is to have an interface for the medicine
+ * @author Brandon Foley
+ * @version 1.0
+ */
 public interface Counter {
     public boolean addMedicine();
 }
